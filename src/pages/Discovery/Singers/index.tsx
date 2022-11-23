8bea49f25eab6c12654cae708b00aea534932f55
Lyove <lyove@outlook.com>
2022-11-23 17:58:57 +0800

    Update index.tsx

--- src/pages/Discovery/Singers/index.tsx
+++ src/pages/Discovery/Singers/index.tsx
@@ -2,8 +2,8 @@ import React, { useState, useEffect } from 'react';
 import { Spin } from '@douyinfe/semi-ui';
 import SingerCategory from 'components/SingerCategory';
 import useAsyncFn from 'hooks/useAsyncFn';
-import singerListApis from 'apis/singerList';
-import type { Artist } from 'apis/types/singerList';
+import singerListApis from 'apis/singerlist';
+import type { Artist } from 'apis/types/singerlist';
 
 const Singers = () => {
   const [searchInfo, setSearchInfo] = useState({
